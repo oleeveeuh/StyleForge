4aebee56b9ff6c80c704338db8bbb92afa152f6d
“Olivia <oliau@usc.edu>
2026-01-15 12:20:42 -0800

    Update __init__.py

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,3 +1,19 @@
 """StyleForge benchmarks module"""
 
+from .profiler import (
+    PerformanceProfiler,
+    BenchmarkResult,
+    save_results,
+    load_results
+)
+from .visualize import BenchmarkVisualizer
+
+__all__ = [
+    "PerformanceProfiler",
+    "BenchmarkResult",
+    "BenchmarkVisualizer",
+    "save_results",
+    "load_results",
+]
+
 __version__ = "0.1.0"
