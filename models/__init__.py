a41903468deb6b82deff349f234d5a4f5b21e5ea
“Olivia <oliau@usc.edu>
2026-01-15 10:15:37 -0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,27 @@
 StyleForge - Model Initialization
 
 Exports main model classes for easy importing.
+Optimized models require CUDA kernels and are imported conditionally.
 """
 
 from .conv_blocks import ConvBlock, DeconvBlock, ResidualBlock
 from .style_transfer_net import StyleTransferNetwork
 from .transformer import TransformerBlock
-from .optimized_transformer import OptimizedTransformerBlock
-from .optimized_blocks import OptimizedConvBlock, OptimizedDeconvBlock
-from .optimized_style_transfer_net import OptimizedStyleTransferNetwork, create_optimized_model
+
+# Try to import optimized models (require CUDA kernels)
+try:
+    from .optimized_transformer import OptimizedTransformerBlock
+    from .optimized_blocks import OptimizedConvBlock, OptimizedDeconvBlock
+    from .optimized_style_transfer_net import OptimizedStyleTransferNetwork, create_optimized_model
+    _has_optimized = True
+except ImportError:
+    # CUDA kernels not available
+    OptimizedTransformerBlock = None
+    OptimizedConvBlock = None
+    OptimizedDeconvBlock = None
+    OptimizedStyleTransferNetwork = None
+    create_optimized_model = None
+    _has_optimized = False
 
 __all__ = [
     "StyleTransferNetwork",
@@ -17,11 +30,15 @@ __all__ = [
     "ConvBlock",
     "DeconvBlock",
     "ResidualBlock",
-    "OptimizedTransformerBlock",
-    "OptimizedConvBlock",
-    "OptimizedDeconvBlock",
-    "OptimizedStyleTransferNetwork",
-    "create_optimized_model",
 ]
+
+if _has_optimized:
+    __all__.extend([
+        "OptimizedTransformerBlock",
+        "OptimizedConvBlock",
+        "OptimizedDeconvBlock",
+        "OptimizedStyleTransferNetwork",
+        "create_optimized_model",
+    ])
 
 __version__ = "0.1.0"
